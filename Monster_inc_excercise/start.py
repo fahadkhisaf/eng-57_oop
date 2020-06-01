37fecd4712a069ac85a8ade2cf42eb266c0a1776
fahadkhisaf <fkhisaf@gmail.com>
2020-06-01 23:47:17 +0100

    testing monster inc excercise

--- Monster_inc_excercise/start.py
+++ Monster_inc_excercise/start.py
@@ -3,3 +3,7 @@ from student_monster import *
 from course import *
 
 print('Congratulations, you have succesfully been enrolled')
+
+john_instance = Monster('john', 758, 'orange')
+
+print(john_instance.get_fur())
